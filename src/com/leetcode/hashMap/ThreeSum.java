118260bde69a54c31f00e3f537fa75612bab32e1
MasonTW <mingxin.wang@thoughtworks.com>
2024-01-24 00:25:03 +0800

    fix bug

--- src/com/leetcode/hashMap/ThreeSum.java
+++ src/com/leetcode/hashMap/ThreeSum.java
@@ -17,7 +17,13 @@ public class ThreeSum {
         ArrayList<List<Integer>> result = new ArrayList<>();
         if (sortedNums.isEmpty() || sortedNums.get(0) > 0) return result;
         for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && sortedNums.get(i) == sortedNums.get(i - 1)) continue;
+            /**
+             * Java为了优化性能，在-128到127范围内的Integer值会被缓存。
+             * 当你使用这个范围内的值创建Integer对象时，Java实际上会返回指向相同对象的引用
+             * 如果这些值是通过自动装箱创建的（即将基本类型int转换为Integer）。
+             * 因此，在这个范围内，使用==比较两个Integer对象通常会得到正确的结果
+             **/
+            if (i > 0 && sortedNums.get(i).equals(sortedNums.get(i - 1))) continue;
             int start = i + 1;
             int end = sortedNums.size() - 1;
             while (start < end) {
